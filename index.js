85c49cfc5186b241db106056e0122ba9b4aea6fc
Ivana Stojadinović <ivanastojadinovic001@gmail.com>
2024-02-21 21:18:23 +0100

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { PORT, mongoDBURL } from './config.js';
+import { PORT, MONGO_URI } from './config.js';
 import mongoose from 'mongoose';
 import  booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
@@ -12,15 +12,8 @@ app.use(cors({
     origin: ["http://localhost:5555", "https://mernbookstore.onrender.app"]
 }));
 
-//server production assets
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join('frontend/build')));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-    });}
-
 mongoose
-    .connect(mongoDBURL)
+    .connect(MONGO_URI)
     .then(() => {
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
