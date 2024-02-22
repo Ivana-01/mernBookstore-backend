017c6c638cab33a1dfe002514ea0943421365b57
Ivana Stojadinović <ivanastojadinovic001@gmail.com>
2024-02-22 12:13:40 +0100

    * origin test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const MONGO_URI = process.env.MONGO_URI;
 //middleware for req body and cors policy
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://mernbookstore-mfvi.onrender.com"]
+    origin: '*'
 }));
 
 app.get('/', (req, res) => {
