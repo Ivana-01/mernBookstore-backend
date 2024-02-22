75f6b70ad53cc81a0a9fa16282ad52a92d3b8f8d
Ivana Stojadinović <ivanastojadinovic001@gmail.com>
2024-02-22 12:29:53 +0100

    cors

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 //middleware for req body and cors policy
 app.use(express.json());
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors());
 
 app.get('/', (req, res) => {
     console.log('Hello World!');
